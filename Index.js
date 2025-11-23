8fb56161677a437524bbba6bb759b5c99e819672
DarkPlayz98 <baddampujithareddy19@gmail.com>
2025-11-23 20:36:35 +0530

    Initialize package.json for security-leveling-bot

---
+++ Index.js
@@ -0,0 +1,17 @@
+{
+  "name": "security-leveling-bot",
+  "version": "1.0.0",
+  "description": "A Discord security + leveling bot",
+  "main": "index.js",
+  "type": "commonjs",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "engines": {
+    "node": "18.x"
+  },
+  "dependencies": {
+    "discord.js": "^14.14.1",
+    "dotenv": "^16.3.1"
+  }
+}
